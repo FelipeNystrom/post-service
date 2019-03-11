ed411ccf8b8c90e9302dd1b1f0d7ef9cd8781199
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2019-03-11 09:26:07 +0100

    add sanitze as middleware and refactor db export

--- serverConfig/index.js
+++ serverConfig/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const sanitize = require('sanitize');
 
 module.exports = server => {
+  server.use(sanitize.middleware);
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
 };
